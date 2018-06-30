64f0242a98264542ab0369c6c85d1a3c1d40a911
michlf <michel.failing@gmail.com>
2018-06-30 15:43:05 +0200

    First commit

---
+++ Examples/Examples.py
@@ -0,0 +1,41 @@
+import numpy as np
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+sns.set(style="whitegrid", color_codes=True)
+
+np.random.seed(sum(map(ord, "categorical")))
+
+# Load data
+titanic = sns.load_dataset("titanic")
+tips = sns.load_dataset("tips")
+iris = sns.load_dataset("iris")
+
+# Do plotting
+#sns.stripplot(x="day", y="total_bill", data=tips)
+#plt.show()
+
+## Boxplots
+sns.boxplot(x="day", y="total_bill", hue="time", data=tips)
+plt.show()
+tips["weekend"] = tips["day"].isin(["Sat", "Sun"])
+sns.boxplot(x="day", y="total_bill", hue="weekend", data=tips, dodge=False)
+plt.show()
+
+# Violinplots
+sns.violinplot(x="total_bill", y="day", hue="time", data=tips)
+plt.show()
+sns.violinplot(x="total_bill", y="day", hue="time", data=tips,
+               bw=.1, scale="count", scale_hue=False)
+plt.show()
+sns.violinplot(x="day", y="total_bill", data=tips, inner=None)
+sns.swarmplot(x="day", y="total_bill", data=tips, color="w", alpha=.5)
+plt.show()
+sns.violinplot(x="day", y="total_bill", hue="sex", data=tips, split=True)
+plt.show()
+sns.violinplot(x="day", y="total_bill", hue="sex", data=tips,
+               split=True, inner="stick", palette="Set3")
+plt.show()
+
+print(tips, np.shape(tips), type(tips))
---
+++ StatsPaper/plottingEffects.py
@@ -0,0 +1,153 @@
+# Import modules
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+import platform
+
+# Set paths and load experiment 1 data
+# Get Dropbox path irrespective of OS
+print(platform.system())
+if platform.system() == "Windows":
+    projectFolder = '/Users/michlf/Dropbox/Work/Data/behavioral/attention/SpatialProb/IndividualPriorityMaps/Experiment2'
+else:
+    projectFolder = '/Users/michlf/Dropbox/Work/Data/behavioral/attention/SpatialProb/IndividualPriorityMaps/Experiment2'
+analysisFolder = projectFolder+'/figures/'
+if not os.path.exists(analysisFolder):
+    os.makedirs(analysisFolder)
+dataFile = '/Data/finalAnalysis/SpatProbExp2_final.xlsx'
+data = pd.read_excel(projectFolder+dataFile, sheet_name=0)
+
+# Styles & color paletts
+# Standard matplotlib colors
+#1f77b4 blue
+#ff7f0e orange
+#2ca02c green
+#d62728 red
+#9467bd purple
+#8c564b brown
+#e377c2 pink
+#7f7f7f dark gray
+#bcbd22 dirty yellow
+#17becf cyan
+
+pTarLoc = {"lowProb": "#1f77b4", "highProbColor": "#2ca02c", "highProbShape": "#d62728"}
+#palettTarLoc = {cond_tarLocation: "r" if cond_tarLocation == "lowProb" else "b" for cond_tarLocation in dataM.cond_tarLocation.unique()}
+pTarLocGrad = {"Dis-0": "#1f77b4", "Dis-1": "#2ca02c", "Dis-2": "#d62728", "Dis-3": "#d62728", "Dis-4": "#d62728"}
+pDisLoc = {"lowProb": "#1f77b4", "highProb": "#2ca02c", "highProbOther": "#d62728"}
+pDisLocGrad = {"Dis-0": "#1f77b4", "Dis-1": "#2ca02c", "Dis-2": "#d62728", "Dis-3": "#d62728", "Dis-4": "#d62728"}
+meanlineprops = dict(linestyle='--', linewidth=2.5, color='red')
+
+### Figure 1
+
+# Data selection
+dataTarLoc = pd.pivot_table(data[(data.cond_disPresent == 'absent') & (data.RTquicker200 == 0)],
+                            values='responseTime', index='subject_nr', columns='cond_tarLocation')
+# dataDisLoc = pd.melt(dataDisLoc)  #if columns is a list (i.e. for e.g. 2 x 2 ANOVAs)
+dataTarLoc = pd.melt(
+    dataTarLoc.reset_index(),
+    id_vars='subject_nr',
+    var_name='cond_tarLocation',
+    value_vars=['highProbColor', 'highProbShape', 'lowProb'],
+    value_name='responseTime')
+
+# Descriptives
+means = dataTarLoc.groupby(['cond_tarLocation'])['responseTime'].mean().values
+#mobs = dataTarLoc['cond_tarLocation'].value_counts().values
+#pos = range(len(mobs))
+
+# Potting
+fig1, (ax) = plt.subplots(1, 1, figsize=(4, 6), dpi=100)
+sns.violinplot(x='cond_tarLocation', y='responseTime', data=dataTarLoc, cut=0, saturation=.2, palette=pTarLoc)
+sns.swarmplot(x="cond_tarLocation", y="responseTime", data=dataTarLoc, color=(1, 1, 1), alpha=.40)
+ax.plot([0, 1, 2], [means[0], means[1], means[2]], color=(.85, 0, 0), marker='s', markersize=7,
+        markeredgecolor=(0, 0, 0), linewidth=3, linestyle='dashed', dashes=(0.75, 0.75))
+# Each condition in violin plot corresponds to 1 px, so use floats because scaling is all messed up.
+#for tick, label in zip(pos, ax.get_xticklabels()):
+    #ax.plot([pos[tick]-0.1, pos[tick]+0.1], [means[tick], means[tick]], color='red', alpha=.75, linewidth=3)  #linestyle='dashed', dashes=(0.75, 0.75))
+sns.despine(offset=10, trim=True)
+sns.set_context('paper')
+ax.set_xlabel("Target Location")
+ax.set_ylabel("Response Time [ms]")
+fig1.savefig(analysisFolder+'figure1.svg', bbox_inches='tight')
+plt.show()
+# # Horizontally
+# sns.violinplot(y='cond_tarLocation', x='responseTime', data=dataTarLoc, cut=1.5)
+# #sns.swarmplot(y="cond_tarLocation", x="accuracy", data=dataTarLoc, color="w", alpha=.40
+# plt.show()
+# # Boxplot
+# sns.boxplot(x='cond_tarLocation', y='responseTime', data=dataTarLoc, orient='v', showmeans=True, meanprops=meanlineprops,
+#             meanline=True, notch=False)
+# sns.swarmplot(x="cond_tarLocation", y="responseTime", data=dataTarLoc, color="black", alpha=.4)
+# plt.show()
+
+### Figure 2
+dataTarLocGrad = pd.pivot_table(data[(data.cond_disPresent == 'absent') & (data.RTquicker200 == 0)],
+                                values='responseTime', index='subject_nr', columns='TarDistanceFromColor')
+dataTarLocGrad = pd.melt(
+    dataTarLocGrad.reset_index(),
+    id_vars='subject_nr',
+    var_name='TarDistanceFromColor',
+    value_vars=['Dis-0', 'Dis-1', 'Dis-2', 'Dis-3', 'Dis-4'],
+    value_name='responseTime')
+
+# Descriptives
+means = dataTarLocGrad.groupby(['TarDistanceFromColor'])['responseTime'].mean().values
+
+# Plotting
+fig2, (ax) = plt.subplots(1, 1, figsize=(6, 6), dpi=100)
+sns.violinplot(x='TarDistanceFromColor', y='responseTime', data=dataTarLocGrad, cut=0, saturation=.2, palette=pTarLocGrad)
+sns.swarmplot(x="TarDistanceFromColor", y="responseTime", data=dataTarLocGrad, color=(1, 1, 1), alpha=.40)
+ax.plot(range(len(means)), [means[0], means[1], means[2], means[3], means[4]], color=(.85, 0, 0), marker='s', markersize=7,
+        markeredgecolor=(0, 0, 0), linewidth=3, linestyle='dashed', dashes=(0.75, 0.75))
+sns.despine(offset=10, trim=True)
+sns.set_context('paper')
+plt.show()
+fig2.savefig(analysisFolder+'figure2.svg', bbox_inches='tight')
+
+### Figure 3
+dataDisLoc = pd.pivot_table(data[(data.cond_disPresent == 'present') & (data.RTquicker200 == 0) & (data.correct == 1)],
+                                values='responseTime', index='subject_nr', columns=['probabilityCorrection_short'])
+dataDisLoc = pd.melt(
+    dataDisLoc.reset_index(),
+    id_vars='subject_nr',
+    var_name='probabilityCorrection_short',
+    value_vars=['highProb', 'highProbOther', 'lowProb'],
+    value_name='responseTime')
+# Descriptives
+means = dataDisLoc.groupby(['probabilityCorrection_short'])['responseTime'].mean().values
+
+# Plotting
+fig3, (ax) = plt.subplots(1, 1, figsize=(4, 6), dpi=100)
+sns.violinplot(x='probabilityCorrection_short', y='responseTime', data=dataDisLoc, cut=0, saturation=.2, palette=pDisLoc)
+sns.swarmplot(x="probabilityCorrection_short", y="responseTime", data=dataDisLoc, color=(1, 1, 1), alpha=.40)
+ax.plot(range(len(means)), [means[0], means[1], means[2]], color=(.85, 0, 0), marker='s', markersize=7,
+        markeredgecolor=(0, 0, 0), linewidth=3, linestyle='dashed', dashes=(0.75, 0.75))
+sns.despine(offset=10, trim=True)
+sns.set_context('paper')
+plt.show()
+fig3.savefig(analysisFolder+'figure3.svg', bbox_inches='tight')
+
+### Figure 4
+dataDisLocGrad = pd.pivot_table(data[(data.cond_disPresent == 'present') & (data.RTquicker200 == 0) & (data.correct == 1)],
+                                values='responseTime', index='subject_nr', columns=['DisDistance'])
+dataDisLocGrad = pd.melt(
+    dataDisLocGrad.reset_index(),
+    id_vars='subject_nr',
+    var_name='DisDistance',
+    value_vars=['Dis-0', 'Dis-1', 'Dis-2', 'Dis-3', 'Dis-4'],
+    value_name='responseTime')
+# Descriptives
+means = dataDisLocGrad.groupby(['DisDistance'])['responseTime'].mean().values
+
+# Plotting
+fig4, (ax) = plt.subplots(1, 1, figsize=(6, 6), dpi=100)
+sns.violinplot(x='DisDistance', y='responseTime', data=dataDisLocGrad, cut=0, saturation=.2, palette=pDisLocGrad)
+sns.swarmplot(x="DisDistance", y="responseTime", data=dataDisLocGrad, color=(1, 1, 1), alpha=.40)
+ax.plot(range(len(means)), [means[0], means[1], means[2], means[3], means[4]], color=(.85, 0, 0), marker='s', markersize=7,
+        markeredgecolor=(0, 0, 0), linewidth=3, linestyle='dashed', dashes=(0.75, 0.75))
+sns.despine(offset=10, trim=True)
+sns.set_context('paper')
+plt.show()
+fig4.savefig(analysisFolder+'figure4.svg', bbox_inches='tight')
+
