5afc693607e8860616b194054bc0b029814a5ad8
Git Gud <--get>
2019-01-30 00:56:54 +0100

    Update plotsExp1.py

--- StatsMultiplePaper/plotsExp1.py
+++ StatsMultiplePaper/plotsExp1.py
@@ -224,7 +224,7 @@ plt.ylabel('Error Rate\n [in %]')
 sns.despine(offset=10, trim=True)
 plt.show()
 
-### Save ###
+### Save figures ###
 
 fig1.savefig(analysisFolder+'figure1.svg', bbox_inches='tight')
 fig2.savefig(analysisFolder+'figure2.svg', bbox_inches='tight')
